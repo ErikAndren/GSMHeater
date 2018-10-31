e45e5e8bf4f0267aa04052f6a1ebf7ab02bd96d3
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-10-31 21:34:44 +0100

    Check that the shield is connected to the network

--- HeaterGSM.ino
+++ HeaterGSM.ino
@@ -4,28 +4,66 @@ SoftwareSerial SIM900(7, 8);
 
 char incoming_char = 0;
 
+const String CHECK_NETWORK = "AT+CREG?\r\n";
+const String NETWORK_CONNECTED = "+CREG: 0,1\r\n";
+
 void setup() {
   SIM900.begin(19200);
   Serial.begin(19200);
-  Serial.println("Arduino booted, waiting for hello from SIM900");
 
   delay(1000);
-
-  SIM900.println("AT");
   
-  //SIM900.println("ATDT0700838506");
+  waitForNetwork();
 }
 
-void loop() {
-    if (SIM900.available()) {
+int readSIM900UART() {
+  if (SIM900.available()) {
       //Get the character from the cellular serial port
       incoming_char = SIM900.read(); 
       //Print the incoming character to the terminal
-      Serial.print(incoming_char); 
-    }
+      //Serial.print(incoming_char);
+      return incoming_char;
+  }
+  return -1;
+}
 
-    if (Serial.available()) {
+void writeSIM900UART(byte val) { 
+}
+
+void loop() {
+  readSIM900UART();
+  
+  if (Serial.available()) {
       incoming_char = Serial.read();
       SIM900.write(incoming_char);
     }
 }
+
+void waitForNetwork() {
+  String result = "";
+  
+  SIM900.print(CHECK_NETWORK);
+  Serial.println("");
+  while (true) {
+    if (readSIM900UART() >= 0) {
+      result += incoming_char;
+
+      if (incoming_char == '\n') {        
+        if (result.compareTo(NETWORK_CONNECTED) == 0) {
+          Serial.println("Connected to network");
+          return;
+        } else if (result.compareTo(CHECK_NETWORK) == 0) { 
+          result = "";
+        } else if (result.compareTo("\r\n") == 0) {
+          result = "";
+        } else {
+          return;
+          result = "";
+          delay(1000);
+          SIM900.println(CHECK_NETWORK);
+        }
+      }
+    }
+  }
+}
+
