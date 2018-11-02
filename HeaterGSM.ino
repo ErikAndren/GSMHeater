ace10eb177f96a6e80c93ff663e06d06d47e3f62
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-11-02 19:55:41 +0100

    Start to implement GPRS setup

--- HeaterGSM.ino
+++ HeaterGSM.ino
@@ -6,10 +6,34 @@ char incoming_char = 0;
 
 const String CHECK_NETWORK = "AT+CREG?\r\n";
 const String NETWORK_CONNECTED = "+CREG: 0,1\r\n";
+const String SHUT_IP_CONNECTION = "AT+CIPSHUT\r\n";
+const String CHECK_IP_STATUS = "AT+CIPSTATUS\r\n";
+const String IP_STATUS_SHUTDOWN = "SHUT OK\r\n";
+
+const String SINGLE_CONNECTION_MODE = "AT+CIPMUX=0\r\n";
+const String READ_PDP_CONTEXT = "AT+CGDCONT?\r\n";
+const String WRITE_PDP_CONTEXT = "AT+CGDCONT=1,\"IP\",\"4g.tele2.se\"\r\n";
+const String SET_APN_USER_PWD = "AT+CSTT=\"4g.tele2.se\"\r\n";
+const String BRING_UP_GPRS = "AT+CIICR\r\n";
+const String GET_LOCAL_IP = "AT+CIFRS\r\n";
+const String CONNECT_TO_SERVER = "AT+CIPSTART=\"TCP\",\"home.zachrisson.info\",\"6666\"\r\n";
+const String SEND_DATA = "AT+CIPSEND\r\n";
+const String TURN_OFF_ECHO = "ATE0\r\n";
 
 void setup() {
+  //FIXME: Add possibility to start shield from arduino
+//  pinMode(9, OUTPUT); 
+//  digitalWrite(9,LOW);
+//  delay(1000);
+//  digitalWrite(9,HIGH);
+//  delay(2500);
+//  digitalWrite(9,LOW);
+//  delay(3500);
+  
   SIM900.begin(19200);
   Serial.begin(19200);
+
+  Serial.print(TURN_OFF_ECHO);
   
   waitForNetwork();
 }
@@ -26,7 +50,7 @@ int readSIM900UART(boolean echo) {
   return -1;
 }
 
-void writeSIM900UART(byte val) { 
+void writeSIM900UART(byte val) {   
 }
 
 void loop() {
@@ -36,6 +60,21 @@ void loop() {
       incoming_char = Serial.read();
       SIM900.write(incoming_char);
     }
+}
+
+// FIXME: Must implement a larger state machine
+void setupGPRS() {
+  String result = "";
+  SIM900.write(SHUT_IP_CONNECTION);
+  while (true) {
+    if (readSIM900UART(false) > 0) {
+      result += incoming_char;
+      if (incoming_char == '\n') {
+        result.compareTo(IP_STATUS_SHUTDOWN) 
+      }
+      
+    }
+  }
 }
 
 void waitForNetwork() {
