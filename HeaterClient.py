905aba1962d88ffe8d839024d79af5ab3b8d23f4
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-11-04 19:47:35 +0100

    Stop relay timer

--- HeaterClient.py
+++ HeaterClient.py
@@ -28,9 +28,10 @@ class deactivate_relay_timer(Thread):
     def run(self):
         while not self.stopped.wait(max_active_s):
             global client
+            global stopFlag2
             print 'Deactivating relay due to max timer'
             relay_state = '0'
-            self.set()
+            stopFlag2.set()
 
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,24 +39,22 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 client.connect(('home.zachrisson.info', 6666))
 
-#def deactivate_relay():
-#    print 'max timeout, deactivating relay'
-#    relay_state = '0'
-
 stopFlag = Event()
 t = heartbeat_timer(stopFlag)
 t.start()
 
 stopFlag2 = Event()
-t2 = deactivate_relay_timer(t2)
+t2 = deactivate_relay_timer(stopFlag2)
+
+client.send(relay_state)
 
 while True:
     response = client.recv(1)
 
     if response[0] == '0':
         relay_state = '0'
-        print 'turning off relay'
+        print 'Turning off relay'
     elif response[0] == '1':
-        print 'turning on relay'
+        print 'Turning on relay'
         relay_state = '1'
         t2.start()
